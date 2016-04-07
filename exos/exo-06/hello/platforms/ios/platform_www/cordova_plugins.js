f120ae15a4c41d13b6986cdc20fd0bfb3333936a
Bachelor 3 <bachelor3@Mac-3c075418fd90.fro-207.froment.in.iesanetwork.com>
2016-04-07 17:00:32 +0200

    Add exo06

---
+++ exos/exo-06/hello/platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.1",
+    "cordova-plugin-device": "1.1.1"
+}
+// BOTTOM OF METADATA
+});
---
+++ exos/exo-06/hello/platforms/ios/www/js/index.js
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+
+
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+        document.getElementById('timestamp').innerHTML = new Date();
+
+        var element = document.getElementById('deviceProperties');
+        element.innerHTML = '<ul data-role="listview" id="list">'
+            + '<li data-icon="arrow-l">Name: ' + device.name + '</li>'
+            + '<li data-icon="arrow-r">Model: ' + device.model + '</li>'
+            + '<li data-icon="arrow-r">Cordova: ' + device.cordova + '</li>'
+            + '<li data-icon="arrow-l">Platform: ' + device.platform + '</li>'
+            + '<li data-icon="arrow-r">UUID: ' + device.uuid + '</li>'
+            + '<li data-icon="arrow-l">Version: ' + device.version + '</li>'
+            + '</ul>'
+        ;
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+
+app.initialize();
